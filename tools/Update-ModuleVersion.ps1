39df63c1199c00f4fa87d6e618080991f5a9ac51
Vercellone, Jason <Jason.Vercellone@mkcorp.com>
2025-02-08 22:28:36 -0600

    Use tools\Update-ModuleVersion.ps1

---
+++ tools/Update-ModuleVersion.ps1
@@ -0,0 +1,26 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory)]
+    [string]$Version,
+    
+    [Parameter(Mandatory)]
+    [string]$Path
+)
+
+$ErrorActionPreference = 'Stop'
+Write-Host "Processing version: $Version"
+
+if($Version -match '(\d+\.\d+\.\d+)(-[a-zA-Z0-9\.]+)?') {
+    $moduleVersion = $matches[1]
+    Write-Host "Module version: $moduleVersion"
+    $prerelease = if($matches[2]){($matches[2]).TrimStart('-')}else{$null}
+    Write-Host "Prerelease: $prerelease"
+    
+    Update-ModuleManifest -Path $Path -ModuleVersion $moduleVersion
+    if($prerelease) {
+        Update-ModuleManifest -Path $Path -Prerelease $prerelease
+    }
+} else {
+    Write-Error "Invalid version format: $Version"
+    exit 1
+}
